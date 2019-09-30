51edabe3adf44c2602a0b9ca876832dda922128b
rjgates3 <gates.j.richard@gmail.com>
2019-09-30 11:49:04 -0600

    updated fib

--- fibonacci.js
+++ fibonacci.js
@@ -3,14 +3,14 @@
 //7 create fibArr with 7 numbers
 //7 6 5 4 3 2 1
 
+let solution = [1, 1];
 
 function fibonacci(num) {
     if(num < 2) {
-        return num;
+        return solution;
     }
-    return fibonacci(num-1) + fibonacci(num-2);
+    solution.push(solution[solution.length-1] + solution[solution.length-2]);
+    return fibonacci(num-1);
 }
 
 console.log(fibonacci(7)); //13
-console.log(fibonacci(6)); //8
-console.log(fibonacci(8)); //21
